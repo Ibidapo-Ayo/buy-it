a8717fc3cf818ce1f666fc25b6d2ef6efa93f54a
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-10-11 18:38:54 -0700

    Minor refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-const path = require('path');
+import path from "path";
+
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve.alias['@'] = path.resolve(__dirname, 'app');
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "app");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
