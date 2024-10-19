98e7b7a7bc6f8cf04e658630612b7c922379f358
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-10-19 16:15:26 -0700

    Minor updates to the header component

--- components/landing-page/nav/Header.tsx
+++ components/landing-page/nav/Header.tsx
@@ -5,13 +5,16 @@ import Sidebar from '../Sidebar'
 import NavbarActions from './NavbarActions'
 import SearchComponent from './SearchComponet'
 import { useProducts } from '@/app/context/product-context'
+import Link from 'next/link'
 
 const Header = () => {
-    const {totalCarts} = useProducts()
+    const { totalCarts } = useProducts()
     return (
         <div className='w-full grid grid-cols-[auto,1fr,auto] justify-between items-center py-3 gap-4'>
             <div className='w-[60px] md:w-[150px]'>
-                <Image src="/images/logo.png" alt="alt" width={1000} height={1000} className='w-24 md:w-28' />
+                <Link href="/">
+                    <Image src="/images/logo.png" alt="alt" width={1000} height={1000} className='w-24 md:w-28' />
+                </Link>
             </div>
             <SearchComponent />
 
--- components/landing-page/nav/NavbarActions.tsx
+++ components/landing-page/nav/NavbarActions.tsx
@@ -9,28 +9,23 @@ type NavActionProps = {
 }
 
 
-const NavbarActions = ({totalCarts}: NavActionProps) => {
+const NavbarActions = ({ totalCarts }: NavActionProps) => {
     return (
-        <div className='w-full md:flex md:flex-row flex-col  justify-between items-center gap-5'>
-            <Button variant={"ghost"} className='bg-transparent flex items-center space-x-2'>
-                <Image src={icons.user} alt='user icon' width={1000} height={1000} className='w-6' />
-                <span className='font-medium text-sm'>Account</span>
-            </Button>
-            <Button variant={"ghost"} className='bg-transparent flex items-center space-x-2  relative'>
-                <Image src={icons.favourite} alt='user icon' width={1000} height={1000} className='w-6' />
-                <span className='font-medium text-sm'>Wishlist</span>
-                <div className='w-4 h-4 p-[9px] rounded-full absolute top-[2px] left-3 flex items-center justify-center  bg-red-500'>
-                    <span className='text-[10px] text-white font-semibold'>0</span>
-                </div>
-            </Button>
+        <div className='w-full md:flex md:flex-row flex-col  justify-between items-center'>
+            <Link href={"/accounts"}>
+                <Button variant={"ghost"} className='bg-transparent flex items-center space-x-2'>
+                    <Image src={icons.user} alt='user icon' width={1000} height={1000} className='w-6' />
+                    <span className='font-medium text-sm'>Account</span>
+                </Button>
+            </Link>
             <Link href={"/cart"}>
-            <Button variant={"ghost"} className='bg-transparent flex items-center space-x-2 relative'>
-                <Image src={icons.shop} alt='user icon' width={1000} height={1000} className='w-6' />
-                <span className='font-medium text-sm'>Your cart</span>
-                <div className='w-4 h-4 p-[9px] rounded-full absolute top-[2px] left-3 flex items-center justify-center  bg-red-500'>
-                    <span className='text-[9px] text-white font-semibold'>{totalCarts}</span>
-                </div>
-            </Button>
+                <Button variant={"ghost"} className='bg-transparent flex items-center space-x-2 relative'>
+                    <Image src={icons.shop} alt='user icon' width={1000} height={1000} className='w-6' />
+                    <span className='font-medium text-sm'>Your cart</span>
+                    <div className='w-4 h-4 p-[9px] rounded-full absolute top-[2px] left-5 flex items-center justify-center  bg-red-500'>
+                        <span className='text-[9px] text-white font-semibold'>{totalCarts}</span>
+                    </div>
+                </Button>
             </Link>
         </div>
     )
